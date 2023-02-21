d7d49174ee3a549d259a67918890d9176f19909f
sps-pashka-it <stetsyuk.p.s@gmail.com>
2023-02-21 15:04:17 +0200

    finish correct

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,61 +1,61 @@
 import lodashThrottle from 'lodash.throttle';
 
-const STORAGE_KEY = "feedback-form-state"
+const STORAGE_KEY = 'feedback-form-state';
 
 const feedbackForm = document.querySelector('.feedback-form');
 const dataForm = {
-    email: '',
-    message: '',
+  email: '',
+  message: '',
 };
 populateEmailTextarea();
 
-feedbackForm.addEventListener('submit', onFormSubmit );
-feedbackForm.addEventListener('input', lodashThrottle(onEmailTextareaInput, 500));
+feedbackForm.addEventListener('submit', onFormSubmit);
+feedbackForm.addEventListener(
+  'input',
+  lodashThrottle(onEmailTextareaInput, 500)
+);
 
 function onFormSubmit(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (dataForm.email !== '' && dataForm.message !== '') {
-        e.currentTarget.reset();
-        localStorage.removeItem(STORAGE_KEY);
-    } else {
-        alert('Будь ласка, заповніть всі поля!');
-    };
-};
-
-function onEmailTextareaInput(e) {
-    const saveMassage = localStorage.getItem(STORAGE_KEY);
-    if (saveMassage) {
-        const saveMassageObject = JSON.parse(saveMassage);
-        if (saveMassageObject.email !== '') {
-            dataForm.email = saveMassageObject.email;
-            };
-        if (saveMassageObject.message !== '') {
-                dataForm.message = saveMassageObject.message;
-            };
-    }
-
-    dataForm[e.target.name] = e.target.value;
-
-
-    console.log(dataForm);
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
-    console.log(JSON.stringify(dataForm));
+  if (dataForm.email !== '' && dataForm.message !== '') {
+      console.log(dataForm);
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    dataForm.email = '';
+    dataForm.message = '';
+  } else {
+    alert('Будь ласка, заповніть всі поля!');
+  }
 }
 
+function onEmailTextareaInput(e) {
+  const saveMassage = localStorage.getItem(STORAGE_KEY);
+  if (saveMassage) {
+    const saveMassageObject = JSON.parse(saveMassage);
+    if (saveMassageObject.email !== '') {
+      dataForm.email = saveMassageObject.email;
+    };
+    if (saveMassageObject.message !== '') {
+      dataForm.message = saveMassageObject.message;
+    };
+  };
+
+  dataForm[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
+};
+
 function populateEmailTextarea() {
-    const saveMassage = localStorage.getItem(STORAGE_KEY);
-    console.log(saveMassage);
-    if (saveMassage) {
-        const saveMassageObject = JSON.parse(saveMassage);
-        console.log(saveMassageObject);
-        if (saveMassageObject.email !== '') {
-            const email = document.querySelector('input');
-            email.value = saveMassageObject.email;
-            };
-            if (saveMassageObject.message !== '') {
-                const textarea = document.querySelector('textarea');
-                textarea.value = saveMassageObject.message;
-            };
-    }
+  const saveMassage = localStorage.getItem(STORAGE_KEY);
+  if (saveMassage) {
+    const saveMassageObject = JSON.parse(saveMassage);
+    if (saveMassageObject.email !== '') {
+      const email = document.querySelector('input');
+      email.value = saveMassageObject.email;
+    };
+    if (saveMassageObject.message !== '') {
+      const textarea = document.querySelector('textarea');
+      textarea.value = saveMassageObject.message;
+    };
+  };
 };
